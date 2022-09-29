a5289d0b6db0791d41ce271c0361c57a8a5c05a5
Ferdy199 <fdumbadumba@gmail.com>
2022-09-29 13:36:09 +0700

    memperbaiki layouting

--- app/src/main/java/com/daftech/sweetectapp/ui/dashboard/DashboardActivty.kt
+++ app/src/main/java/com/daftech/sweetectapp/ui/dashboard/DashboardActivty.kt
@@ -18,7 +18,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -28,8 +27,6 @@ import com.daftech.sweetectapp.core.adapter.HistoryAdapter
 import com.daftech.sweetectapp.core.utils.ViewModelFactory
 import com.daftech.sweetectapp.databinding.ActivityDashboardBinding
 import com.daftech.sweetectapp.ui.detail.DetailActivity
-import com.daftech.sweetectapp.ui.main.MainActivity
-import com.daftech.sweetectapp.ui.onboarding.ViewPagerFragment
 import com.daftech.sweetectapp.ui.signin.SignInActivity
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
